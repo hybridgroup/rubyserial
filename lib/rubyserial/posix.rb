254aeef404297606327ed12b39eea1e78d016d7b
hunterboerner <hunterboerner@gmail.com>
2014-07-29 14:37:58 -0500

    Fine, you want size instead of length

--- lib/rubyserial/posix.rb
+++ lib/rubyserial/posix.rb
@@ -89,7 +89,7 @@ class Serial
     loop do
       current_byte = getbyte
       bytes << current_byte unless current_byte.nil?
-      break if (bytes.last(sep.bytes.length) == sep.bytes) || ((bytes.size == limit) if limit)
+      break if (bytes.last(sep.bytes.size) == sep.bytes) || ((bytes.size == limit) if limit)
     end
 
     bytes.map { |e| e.chr }.join
