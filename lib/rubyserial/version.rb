5ff3a857fa6f00cb07acde592265097fedbc6715
deadprogram <ron@hybridgroup.com>
2017-12-09 18:51:07 +0100

    Update version to v0.5.0
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- lib/rubyserial/version.rb
+++ lib/rubyserial/version.rb
@@ -1,7 +1,7 @@
-# Copyright (c) 2014-2016 The Hybrid Group
+# Copyright (c) 2014-2017 The Hybrid Group and Friends
 
 module RubySerial
   unless const_defined?('VERSION')
-    VERSION = "0.4.0"
+    VERSION = "0.5.0"
   end
 end
